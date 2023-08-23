779de7a6f8df649c837d4fb6aa73942dc8fb9540
dineshsaini777 <dinesh016622@gmail.com>
2023-08-23 10:05:19 -0400

    changes

--- assets/js/add.js
+++ assets/js/add.js
@@ -1,7 +1,9 @@
-let p = document.querySelector("p")
+let p = document.querySelector("#p")
 let add = document.querySelector(".add")
 let sub = document.querySelector(".sub")
-let multiply = document.querySelector(".multiply")
+let reset = document.querySelector(".reset")
+let save = document.querySelector(".save")
+let total = document.querySelector("#total")
 console.log(p)
 add.addEventListener("click", function () {
     p.innerHTML = +p.innerHTML + 1;
@@ -9,6 +11,25 @@ add.addEventListener("click", function () {
 sub.addEventListener("click", function () {
     p.innerHTML = +p.innerHTML - 1;
 })
-multiply.addEventListener("click", function () {
-    p.innerHTML = +p.innerHTML * 2;
+
+
+reset.addEventListener("click", function () {
+    p.innerHTML = 0
+    total.innerHTML = 0
 })
+save.addEventListener("click", function () {
+    if (p.innerHTML == 0) {
+        save.classlist.add("disable")
+        alert("notsave 0!!!")
+    }
+    else if (p.innerHTML != 0) {
+        if (total.innerHTML == 0) {
+            total.innerHTML = p.innerHTML
+        }
+        else if (total.innerHTML != 0) {
+            total.innerHTML += "," + p.innerHTML
+        }
+    }
+    p.innerHTML = 0
+})
+
